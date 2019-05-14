fa73e73816a8b44b6d2f4412c6509246f9ffac24
tangming <tangming.HL@gmail.com>
2019-05-14 17:31:16 +0800

    leetcode99.Recover Binary Search Tree

---
+++ cpp/leetcode99.cpp
@@ -0,0 +1,22 @@
+// leetcode99.Recover Binary Search Tree
+class Solution {
+public:
+	TreeNode *p1, *p2, *pre;
+	void recoverTree(TreeNode* root) {
+		traverse(root);
+		int temp = p1->val;
+		p1->val = p2->val;
+		p2->val = temp;
+	}
+	void traverse(TreeNode* root) {
+		if(root) {
+			traverse(root->left);
+			if(pre != nullptr && pre->val > root->val) {
+				if(p1 == nullptr) p1 = pre;
+				p2 = root;
+			}
+			pre = root;
+			traverse(root->right);
+		}
+	}
+};
